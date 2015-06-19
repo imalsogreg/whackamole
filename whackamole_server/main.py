0a83c2fddbd9fe4674394ba451c8715d10ebd77d
Greg Hale <imalsogreg@gmail.com>
2015-06-18 20:07:27 -0400

    some working endpoints for score report

--- whackamole_server/main.py
+++ whackamole_server/main.py
@@ -1,4 +1,17 @@
-from flask import Flask
+from flask import Flask, request
+from flask import json
+import MySQLdb
+import ConfigParser, os
+
+config = ConfigParser.ConfigParser()
+config.readfp(open('db.cfg'))
+
+db = MySQLdb.connect(host   = config.get('Db','host'),
+                     user   = config.get('Db','user'),
+                     passwd = config.get('Db','passwd'),
+                     db     = config.get('Db','db'))
+cur = db.cursor()
+
 app = Flask(__name__)
 with open('db.cfg') as f:
     config = f.read()
@@ -11,6 +24,27 @@ def index():
 def game(difficulty):
     return 'Game difficulty %d' % difficulty
 
-@app.route('/report/<')
+@app.route('/report', methods=['POST','GET'])
+def report():
+    if request.method == 'POST':
+        payload = request.get_json()
+        try:
+            cur.execute("INSERT INTO scores(username,score,difficulty) VALUES (%s,%s,%s);",
+                        (payload["username"],payload["score"],payload["difficulty"]))
+            return "Ok!\n"
+        except:
+            return "SQL error"
+    if request.method == 'GET':
+        cur.execute("SELECT * FROM scores;")
+        rows = cur.fetchall()
+        a = ""
+        for row in rows:
+            for col in row:
+                a = a + str(col) + " "
+            a = a + "\n"
+        return a
+
+
 if __name__ == "__main__":
-   app.run()
+    app.debug = True
+    app.run()
